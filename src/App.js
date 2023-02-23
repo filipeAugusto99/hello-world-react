7b8f0e5340b7b4b1a671f196c3183454f6cc1159
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-23 16:46:46 -0300

    Component reuse

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react"
 import Header from "./Header"
 import Content from "./Content"
 
+import Cards from "./Cards"
+import Card from "./Card"
+
+import reactImg from "./imagens/react.png"
+import nodejsImg from "./imagens/nodejs.png"
+import nextjsImg from "./imagens/nextjs.png"
 
 function App() {
   return (
@@ -13,6 +19,26 @@ function App() {
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+        <Cards>
+          <Card
+            imagem={reactImg}
+            titulo="Titulo 1"
+            texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+
+          <Card
+            imagem={nodejsImg}
+            titulo="Titulo 1"
+            texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+
+          <Card
+            imagem={nextjsImg}
+            titulo="Titulo 1"
+            texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+        </Cards>
       </Content>
     </>
   )
